310189eafe04db535e84ce81b2a21bd85ed16f9a
Jakob J. Kolb <kolb@hardfork.io>
2021-06-02 12:37:34 +0200

    Cleanup sass and scss.

--- next.config.js
+++ next.config.js
@@ -1,9 +1,8 @@
 const withPlugins = require('next-compose-plugins')
 const withImages = require('next-images')
-const withSass = require('@zeit/next-sass')
 const path = require('path')
 
-module.exports = withPlugins([[withSass], [withImages]], {
+module.exports = withPlugins([[withImages]], {
   webpack(config, { isServer }) {
     config.resolve.modules.push(path.resolve('./'))
     if (!isServer) {
--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,5 @@
-module.exports = {
-  trailingComma: 'none',
-  tabWidth: 2,
-  semi: false,
-  singleQuote: true
-}
+export const trailingComma = 'none';
+export const tabWidth = 4;
+export const semi = false;
+export const singleQuote = true;
+
